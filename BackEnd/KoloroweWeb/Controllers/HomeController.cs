87d4a6f3d3cc57c602c09836eac8980523146e96
LAPTOP-BIR7OQ53\warwe <warwelong@gmail.com>
2024-10-28 17:55:09 +0100

    added nugget packages, refactoring

---
+++ BackEnd/KoloroweWeb/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoloroweWeb.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserPostController : ControllerBase
+    {
+        private readonly DBContext DBContext;
+
+        public UserPostController(DBContext DBContext)
+        {
+            this.DBContext = DBContext;
+        }
+  
+     }
+}
--- BackEnd/KoloroweWeb/obj/Debug/net8.0/KoloroweWeb.AssemblyInfo.cs
+++ BackEnd/KoloroweWeb/obj/Debug/net8.0/KoloroweWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoloroweWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f76190bb54319b9a96383ef482863701cc2a97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e628e457b68b8927c96d739f268aca4bf156ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoloroweWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoloroweWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
